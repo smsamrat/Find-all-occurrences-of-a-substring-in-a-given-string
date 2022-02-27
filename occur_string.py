e0eb8c43878bd2b9c4ca7c802f287dd1e857dd53
S.M.Samrat <freefire808709@gmail.com>
2022-02-27 23:35:03 +0600

    Initial commit

---
+++ occur_string.py
@@ -0,0 +1,10 @@
+
+str1=input("Enter string value as your wish: ")
+occur_str2=input("Enter your value to find all occurrences of a substring in a given string : ")
+lower_str1 = str1.lower()
+lower_occur_str2 = occur_str2.lower()
+count =lower_str1.count(lower_occur_str2)
+print("The total count is:", count)
+
+
+ 
